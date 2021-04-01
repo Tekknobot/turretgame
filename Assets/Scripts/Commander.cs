9fcc6d33ee27596dc40b4b9d327c2b7d28368a90
Tekknobot <thrillatron@hotmail.com>
2021-04-01 15:45:42 -0400

    Add mechzilla animation.

--- Assets/Scripts/Commander.cs
+++ Assets/Scripts/Commander.cs
@@ -17,7 +17,6 @@ public class Commander : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;
         StartCoroutine(Dialogue());
     }
 
--- Assets/Scripts/TriggerMusic.cs
+++ Assets/Scripts/TriggerMusic.cs
@@ -32,7 +32,7 @@ public class TriggerMusic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
